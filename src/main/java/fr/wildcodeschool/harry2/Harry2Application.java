2bafbfd2e4a166c841694b59b20b002544894662
Bastien Krettly <bastien@wildcodeschool.fr>
2018-12-10 14:02:18 +0100

    Update Harry2Application.java

--- src/main/java/fr/wildcodeschool/harry2/Harry2Application.java
+++ src/main/java/fr/wildcodeschool/harry2/Harry2Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Harry2Application {
 
 	public static void main(String[] args) {
