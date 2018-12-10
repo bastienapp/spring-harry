efd2087b8293e7603858bf08f41be3818920bf96
bastienwcs <bastien@wildcodeschool.fr>
2018-12-10 14:03:40 +0100

    fix

--- src/main/java/fr/wildcodeschool/harry2/Harry2Application.java
+++ src/main/java/fr/wildcodeschool/harry2/Harry2Application.java
@@ -2,6 +2,7 @@ package fr.wildcodeschool.harry2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
